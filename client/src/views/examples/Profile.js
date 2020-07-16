957571dacd984698ee4d9bb6e3e53cd7be46dff6
jideabdqudus <jideabdqudus@gmail.com>
2020-07-16 21:42:16 +0100

    Added commit

--- client/src/views/examples/Profile.js
+++ client/src/views/examples/Profile.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 // reactstrap components
-import { Button, Card, Container, Row, Col, Badge } from "reactstrap";
+import {
+  Button,
+  Card,
+  Container,
+  Row,
+  Col,
+  Badge,
+  CardBody,
+  TabPane,
+  TabContent,
+} from "reactstrap";
 
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
@@ -82,15 +92,11 @@ class Profile extends React.Component {
                       </div>
                     </Col>
                     <Col className="order-lg-1" lg="4">
-                      <div className="card-profile-stats d-flex justify-content-center">
-                        
-                      </div>
+                      <div className="card-profile-stats d-flex justify-content-center"></div>
                     </Col>
                   </Row>
                   <div className="text-center mt-5">
-                    <h3>
-                      Jessica Jones{" "}
-                    </h3>
+                    <h3>Jessica Jones </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
                       Bucharest, Romania
@@ -101,32 +107,32 @@ class Profile extends React.Component {
                     </div>
                     <Row className="justify-content-center">
                       <div>
-                          <Button
-                            className="btn-icon-only rounded-circle"
-                            color="twitter"
-                            href="https://twitter.com/jideabdqudus"
-                            id="tooltip475038071"
-                            target="_blank"
-                          >
-                            <span className="btn-inner--icon">
-                              <i className="fa fa-twitter" />
-                            </span>
-                          </Button>
-                        </div>
-                        
-                        <div>
-                          <Button
-                            className="btn-icon-only rounded-circle ml-1"
-                            color="github"
-                            href="https://github.com/jideabdqudus"
-                            id="tooltip49550721"
-                            target="_blank"
-                          >
-                            <span className="btn-inner--icon">
-                              <i className="fa fa-github" />
-                            </span>
-                          </Button>
-                        </div>
+                        <Button
+                          className="btn-icon-only rounded-circle"
+                          color="twitter"
+                          href="https://twitter.com/jideabdqudus"
+                          id="tooltip475038071"
+                          target="_blank"
+                        >
+                          <span className="btn-inner--icon">
+                            <i className="fa fa-twitter" />
+                          </span>
+                        </Button>
+                      </div>
+
+                      <div>
+                        <Button
+                          className="btn-icon-only rounded-circle ml-1"
+                          color="github"
+                          href="https://github.com/jideabdqudus"
+                          id="tooltip49550721"
+                          target="_blank"
+                        >
+                          <span className="btn-inner--icon">
+                            <i className="fa fa-github" />
+                          </span>
+                        </Button>
+                      </div>
                     </Row>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
@@ -139,9 +145,66 @@ class Profile extends React.Component {
                           giving it a warm, intimate feel with a solid groove
                           structure. An artist of considerable range.
                         </p>
-                        <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                          Show more
-                        </a>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <Col lg="6">
+                      <h1 className="display-4 mb-0">Experience</h1>
+                        <Card>
+                          <CardBody>
+                            <TabContent>
+                              <TabPane>
+                                <h5>Paysmosmo</h5>
+                                <h6>2017/02/10 - Now</h6>
+                                <p className="description">
+                                  <b>Position:</b> Developer
+                                </p>
+                                <p className="description">
+                                  <b>Location:</b> Lagos
+                                </p>
+                                <p className="description">
+                                  <b>Description:</b> eCommerce
+                                </p>
+                              </TabPane>
+                            </TabContent>
+                          </CardBody>
+                          <CardBody>
+                          <TabContent>
+                            <TabPane>
+                              <h5>TechEnd</h5>
+                              <h6>2017/02/10 - Now</h6>
+                              <p className="description">
+                                <b>Position:</b> Developer
+                              </p>
+                              <p className="description">
+                                <b>Location:</b> Lagos
+                              </p>
+                              <p className="description">
+                                <b>Description:</b> eCommerce
+                              </p>
+                            </TabPane>
+                          </TabContent>
+                        </CardBody>
+                        </Card>
+                      </Col>
+                      <Col lg="6">
+                      <h1 className="display-4 mb-0">Education</h1>
+                        <Card>
+                          <CardBody>
+                            <TabContent>
+                              <TabPane>
+                                <h5>University of Lagos</h5>
+                                <h6>2015/02/10 - 2019/10/10</h6>
+                                <p className="description">
+                                  <b>Degree:</b> Bsc
+                                </p>
+                                <p className="description">
+                                  <b>Field of Study:</b> Mathematics
+                                </p>
+                              </TabPane>
+                            </TabContent>
+                          </CardBody>
+                        </Card>
                       </Col>
                     </Row>
                   </div>
