e457b29a585236f97c196755afffcd21b372f15e
jideabdqudus <jideabdqudus@gmail.com>
2020-08-02 20:37:19 +0100

    Added Commit

--- client/src/reducers/dashboardReducer.js
+++ client/src/reducers/dashboardReducer.js
@@ -12,34 +12,34 @@ const initialState = {
       "Lorem Ipsum dolor sit amet - An artist of considerable range, Ryan — the name taken by Melbourne-raised, ",
     twitter: "https://twitter.com/jideabdqudus",
     github: "https://github.com/jideabdqudus",
-    experience: [
-      {
-        company: "Paysmosmo",
-        position: "Developer",
-        location: "Lagos",
-        start: "2019-12-10",
-        end: "now",
-        description: "eCommerce",
-      },
-      {
-        company: "TechEnd",
-        position: "Designer",
-        location: "Lagos",
-        start: "2017-02-10",
-        end: "2019-10-10",
-        description: "Tech",
-      },
-    ],
-    education: [
-      {
-        school: "University of Lagos",
-        degree: "Bsc",
-        start: "2015-02-10",
-        end: "2018-10-10",
-        field: "Mathematics",
-      },
-    ],
   },
+  experience: [
+    {
+      company: "Paysmosmo",
+      position: "Developer",
+      location: "Lagos",
+      start: "2019-12-10",
+      end: "now",
+      description: "eCommerce",
+    },
+    {
+      company: "TechEnd",
+      position: "Designer",
+      location: "Lagos",
+      start: "2017-02-10",
+      end: "2019-10-10",
+      description: "Tech",
+    },
+  ],
+  education: [
+    {
+      school: "University of Lagos",
+      degree: "Bsc",
+      start: "2015-02-10",
+      end: "2018-10-10",
+      field: "Mathematics",
+    },
+  ],
   isAuthenticated: true,
 };
 
@@ -48,7 +48,7 @@ export default (state = initialState, action) => {
     case EDIT_PROFILE:
       return {
         ...state,
-        user: action.payload
+        user: action.payload,
       };
     default:
       return state;
