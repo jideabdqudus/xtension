dd88c943a4e23bd2b80441bfbbc633b42c98c288
jideabdqudus <jideabdqudus@gmail.com>
2020-08-02 14:59:54 +0100

    Added Commit

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
 import profileReducer from "./profileReducer";
+import dashboardReducer from './dashboardReducer'
 
 export default combineReducers({ profileReducer: profileReducer });
